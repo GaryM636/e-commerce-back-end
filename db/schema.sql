b07f1d5bf4f291ed922334568fa89a3f6e30efe9
GaryM636 <142124124+GaryM636@users.noreply.github.com>
2024-01-20 15:29:37 -0800

    fixed schema bug

--- db/schema.sql
+++ db/schema.sql
@@ -1,32 +1,35 @@
--- DROP DATABASE
+-- Drop the database if it exists
 DROP DATABASE IF EXISTS ecommerce_db;
 
--- CREATE DATABASE
+-- Create the database
 CREATE DATABASE ecommerce_db;
 
--- Create Category table
+-- Select the created database
+USE ecommerce_db;
+
+-- Create the Category table
 CREATE TABLE Category (
   id INTEGER PRIMARY KEY AUTO_INCREMENT,
-  category_name STRING NOT NULL
+  category_name VARCHAR(255) NOT NULL
 );
 
--- Create Product table
+-- Create the Product table
 CREATE TABLE Product (
   id INTEGER PRIMARY KEY AUTO_INCREMENT,
-  product_name STRING NOT NULL,
+  product_name VARCHAR(255) NOT NULL,
   price DECIMAL(10, 2) NOT NULL,
   stock INTEGER NOT NULL DEFAULT 10,
   category_id INTEGER,
   FOREIGN KEY (category_id) REFERENCES Category(id)
 );
 
--- Create Tag table
+-- Create the Tag table
 CREATE TABLE Tag (
   id INTEGER PRIMARY KEY AUTO_INCREMENT,
-  tag_name STRING NOT NULL
+  tag_name VARCHAR(255) NOT NULL
 );
 
--- Create ProductTag table
+-- Create the ProductTag table
 CREATE TABLE ProductTag (
   id INTEGER PRIMARY KEY AUTO_INCREMENT,
   product_id INTEGER,
